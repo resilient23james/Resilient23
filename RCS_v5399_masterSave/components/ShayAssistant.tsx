3ada8848e43a49f3379bf7e258ceab25cccfb90b
James moore <resilient23.james@gmail.com>
2025-10-05 01:18:22 -0400

    Delete RCS_v5399_masterSave directory

--- RCS_v5399_masterSave/components/ShayAssistant.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function ShayAssistant(){
-  return (
-    <div className="card">
-      <strong>Shay Assistant</strong>
-      <p>Hi! I can help route your request. Choose Quote, Careers or Contact to begin.</p>
-    </div>
-  );
-}
--- RCS_v5399_masterSave/next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',            // required for `next export`
-  images: { unoptimized: true } // avoids image optimizer on Netlify
-};
-module.exports = nextConfig;
