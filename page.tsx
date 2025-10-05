2b9866fe4bc2875626574c3afa1978721d4ed2f5
James moore <resilient23.james@gmail.com>
2025-10-04 23:35:05 -0400

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = 'force-static';
-export default function Page(){
-  return (
-    <main>
-      <h1>Resilient Commercial Solutions</h1>
-      <p className="lead">One team. Three divisions. Total resilience.</p>
-    </main>
-  );
-}
