21670a3aa4e766e38acc4e0b05d0af9489d09295
James moore <resilient23.james@gmail.com>
2025-10-06 17:38:52 -0400

    Update terms.js

--- app/es/terms.js
+++ app/es/terms.js
@@ -1,1 +1,2 @@
-export default function Page(){return (<section className='section container'><h1 className='h1 mb-4'>Términos de Servicio</h1><p className='sub'>Por favor revisa nuestros términos.</p></section>);}
+import ShayButton from '@/components/ShayButton';
+import ServiceCard from '@/components/ServiceCard';
