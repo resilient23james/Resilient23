59c04dbfa9aabe3d09f9f664b185824a55252d0c
James moore <resilient23.james@gmail.com>
2025-10-06 17:34:32 -0400

    Update about.js

--- app/es/about.js
+++ app/es/about.js
@@ -1,1 +1,2 @@
-export default function Page(){return (<section className='section container'><h1 className='h1 mb-4'>Acerca de Nosotros</h1><p className='sub'>Nuestra misión, valores e historia de liderazgo.</p></section>);}
+import ShayButton from '@/components/ShayButton';
+import ServiceCard from '@/components/ServiceCard';
