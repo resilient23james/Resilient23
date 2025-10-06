2415c4c1a7ba129bccaaaf61802cd42bfda3a060
James moore <resilient23.james@gmail.com>
2025-10-06 17:39:56 -0400

    Update vendor-signup.js

--- app/es/vendor-signup.js
+++ app/es/vendor-signup.js
@@ -1,1 +1,2 @@
-export default function Page(){return (<section className='section container'><h1 className='h1 mb-4'>Únete a la Red de Proveedores</h1><p className='sub'>Cuéntanos sobre tu empresa para unirte a nuestra red.</p></section>);}
+import ShayButton from '@/components/ShayButton';
+import ServiceCard from '@/components/ServiceCard';
