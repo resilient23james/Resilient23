28c1cfa5b1d130d59171020d3d26e5f1f689ebf0
James moore <resilient23.james@gmail.com>
2025-10-06 19:28:43 -0400

    Update layout.js

--- app/es/layout.js
+++ app/es/layout.js
@@ -1,4 +1,5 @@
-import './globals.css';
+import '../globals.css';   // for /app/es/layout.js
+import './globals.css';    // for /app/layout.js (English root)
 import TopBar from '@/components/TopBar';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
