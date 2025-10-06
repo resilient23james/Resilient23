97595d997e75bd24cdc9ee97399459a9b88b31ea
James moore <resilient23.james@gmail.com>
2025-10-06 17:39:27 -0400

    Update testimonials.js

--- app/es/testimonials.js
+++ app/es/testimonials.js
@@ -1,1 +1,2 @@
-export default function Page(){return (<section className='section container'><h1 className='h1 mb-4'>Testimonios</h1><p className='sub'>Lo que dicen nuestros socios.</p></section>);}
+import ShayButton from '@/components/ShayButton';
+import ServiceCard from '@/components/ServiceCard';
