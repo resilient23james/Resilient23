ed2e42975a3a78873249ab1d8af05633974d4db5
James moore <resilient23.james@gmail.com>
2025-10-06 21:24:44 -0400

    Delete components directory

--- components/Logo.jsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-export default function Logo({variant='light', className=''}){
-  const src = variant === 'dark' ? '/logo-dark.png' : '/logo-light.png';
-  return <Image src={src} alt="Resilient Commercial Solutions" width={140} height={36} className={className} />;
-}
